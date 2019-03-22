b39bd1ca89fbde34f9be92c7b679a928790ef111
liujicheng2091 <121757179@qq.com>
2019-03-22 15:13:01 +0800

    dd

--- src/main/java/con_server/ConServerApplication.java
+++ src/main/java/con_server/ConServerApplication.java
@@ -3,10 +3,9 @@ package con_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
+//@EnableEurekaClient
 public class ConServerApplication {
 
 	 public static void main(String[] args) {
